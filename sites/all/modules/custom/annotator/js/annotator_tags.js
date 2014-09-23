bd513a22ad0c2a324c308cf336913a06667d3a65
Mike Widner <mikewidner@stanford.edu>
2014-09-23 19:29:54 +0000

    pre-pull

--- sites/all/modules/custom/annotator/js/annotator_tags.js
+++ sites/all/modules/custom/annotator/js/annotator_tags.js
@@ -2,6 +2,9 @@
   Drupal.behaviors.annotatorTags = {
     attach: function (context, settings) {
       Drupal.Annotator.annotator('addPlugin', 'Tags');
+      // MLW - Autocomplete our tags in the window
+      // @see https://github.com/openannotation/annotator/issues/92#issuecomment-3985124
+      Drupal.Annotator.data('annotator').plugins.Tags.input.autocomplete({source: Drupal.settings.annotator_tags});
     }
   };
 })(jQuery);
