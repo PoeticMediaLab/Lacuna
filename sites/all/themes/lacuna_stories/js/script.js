5987c52846b9fc9a30ac13d8a2deb5c3a2687cbb
vagrant <vagrant@precise32.(none)>
2014-12-04 18:57:40 +0000

    change response css to move imgaes together

--- sites/all/themes/lacuna_stories/js/script.js
+++ sites/all/themes/lacuna_stories/js/script.js
@@ -11,11 +11,62 @@
 // - https://drupal.org/node/1446420
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
-
-
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.my_custom_behavior = {
   attach: function(context, settings) {
+	// $("img").hover(function( event ) {
+	// 	$("img").hide();
+	// });
+	$(window).load(function() {
+	    // $outer_container=$("#outer_container");
+	    $imagePan_panning=$("#imagePan .panning img");
+	    $imagePan=$("img");
+	    $imagePan_container=$("#imagePan .container img");
+	 
+	    $imagePan_panning.css("margin-top",($imagePan.height()-$imagePan_panning.height())/2+"px");
+	    containerWidth=$imagePan.width();
+	    containerHeight=$imagePan.height();
+	    totalContentW=$imagePan_panning.width();
+	    totalContentH=$imagePan_panning.height();
+	    $imagePan_container.css("width",totalContentW).css("height",totalContentH);
+	 
+	    function MouseMove(e){
+	        var mouseCoordsX=(e.pageX - $imagePan.offset().left);
+	        var mouseCoordsY=(e.pageY - $imagePan.offset().top);
+	        var mousePercentX=mouseCoordsX/containerWidth;
+	        var mousePercentY=mouseCoordsY/containerHeight;
+	        var destX=-(((totalContentW-(containerWidth))-containerWidth)*(mousePercentX));
+	        var destY=-(((totalContentH-(containerHeight))-containerHeight)*(mousePercentY));
+	        var thePosA=mouseCoordsX-destX;
+	        var thePosB=destX-mouseCoordsX;
+	        var thePosC=mouseCoordsY-destY;
+	        var thePosD=destY-mouseCoordsY;
+	        var marginL=$imagePan_panning.css("marginLeft").replace("px", "");
+	        var marginT=$imagePan_panning.css("marginTop").replace("px", "");
+	        var animSpeed=500; //ease amount
+	        var easeType="easeOutCirc";
+	        if(mouseCoordsX>destX || mouseCoordsY>destY){
+	            $imagePan_container.stop().animate({left: -thePosA-marginL, top: -thePosC-marginT}, animSpeed,easeType); //with easing
+	        } else if(mouseCoordsX<destX || mouseCoordsY<destY){
+］	            $imagePan_container.stop().animate({left: thePosB-marginL, top: thePosD-marginT}, animSpeed,easeType); //with easing
+	        } else {
+	            $imagePan_container.stop();
+	        }
+	    }
+	 
+	    $imagePan_panning.css("margin-left",($imagePan.width()-$imagePan_panning.width())/2).css("margin-top",($imagePan.height()-$imagePan_panning.height())/2);
+	 
+	    $imagePan.bind("mousemove", function(event){
+	        MouseMove(event);
+	    });
+	});
+
+	$(window).resize(function() {
+	    $imagePan.unbind("mousemove");
+	    $imagePan_container.css("top",0).css("left",0);
+	    $(window).load();
+	});
+
 
     // Place your code here.
 
