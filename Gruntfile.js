0f12700e6448a1ac5a019764c349fbf69af45f96
Ranajay <ranajays@gmail.com>
2014-12-15 00:51:06 -0500

    Created grunt script to autocompile coffee files.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+  	coffee: {
+		compile: {
+				files: [{
+					expand: true,
+					cwd:'sites/all/modules/custom/',
+					src: '**/*.coffee',
+					dest: 'sites/all/modules/custom/',
+					ext: ".js"
+				}]
+  		}
+  	},
+    watch: {
+      files: ['sites/all/modules/custom/**/*.coffee'],
+      tasks: ['newer:coffee']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
+}
+
+
+
+
