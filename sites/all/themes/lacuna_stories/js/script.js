7af7dde68be94213f6d021ecf38f138bba6bc6a9
XiaoYao <xiaoyao@DN51t4dv.SUNet>
2015-01-04 15:32:01 -0800

    add all changes, before cleaning up code

--- sites/all/themes/lacuna_stories/js/script.js
+++ sites/all/themes/lacuna_stories/js/script.js
@@ -17,95 +17,22 @@ Drupal.behaviors.my_custom_behavior = {
 
 $(document).ready(function() {
 	// Tooltip only Text
-// 	$('img').hover(function(){
-//         // Hover over code
-//         var title = $(this).attr('title');
-//         console.log(title)
-//         $(this).data('tipText', title).removeAttr('title');
-//         $('<p class="tooltip"></p>')
-//         .text(title)
-//         .appendTo('body')
-//         .fadeIn('slow');
-// 	}, function() {
-// 	    // Hover out code
-// 	    $(this).attr('title', $(this).data('tipText'));
-// 	    $('.tooltip').remove();
-// 		}).mousemove(function(e) {
-// 	        var mousex = e.pageX + 20; //Get X coordinates
-// 	        var mousey = e.pageY + 10; //Get Y coordinates
-// 			$('.tooltip')
-// 			.css({ top: mousey, left: mousex })
-// 		});
-// });
+	// $('.views-field-field-display-name').hide();
+	// $('.views-field-title').hide();
 
-// $(document).ready(function() {
-// 	$('img').hover(function() {
-// 		$('img').hide();
-//         var title = $(this).attr('title');
-//         console.log(title)
+    $('<span class="text-content"></span>')
+    .appendTo('.views-field-field-image .field-content a');
 
-// 	});
-// });
+	$('img').hover(function(event){
+        // Hover over code
+		// console.log(event.target.nodeName); 
+		console.log(event.target.parentNode);
+		// console.log($(this));
+        // $('<span class="views-field-text"></span>')
+        // .appendTo(event.target.parentNode);
+		});
 
-
-
-
-
-
-
-// 	$(window).load(function() {
-// 	    // $outer_container=$("#outer_container");
-// 	    $imagePan_panning=$("#imagePan .panning img");
-// 	    $imagePan=$("img");
-// 	    $imagePan_container=$("#imagePan .container img");
-	 
-// 	    $imagePan_panning.css("margin-top",($imagePan.height()-$imagePan_panning.height())/2+"px");
-// 	    containerWidth=$imagePan.width();
-// 	    containerHeight=$imagePan.height();
-// 	    totalContentW=$imagePan_panning.width();
-// 	    totalContentH=$imagePan_panning.height();
-// 	    $imagePan_container.css("width",totalContentW).css("height",totalContentH);
-	 
-// 	    function MouseMove(e){
-// 	        var mouseCoordsX=(e.pageX - $imagePan.offset().left);
-// 	        var mouseCoordsY=(e.pageY - $imagePan.offset().top);
-// 	        var mousePercentX=mouseCoordsX/containerWidth;
-// 	        var mousePercentY=mouseCoordsY/containerHeight;
-// 	        var destX=-(((totalContentW-(containerWidth))-containerWidth)*(mousePercentX));
-// 	        var destY=-(((totalContentH-(containerHeight))-containerHeight)*(mousePercentY));
-// 	        var thePosA=mouseCoordsX-destX;
-// 	        var thePosB=destX-mouseCoordsX;
-// 	        var thePosC=mouseCoordsY-destY;
-// 	        var thePosD=destY-mouseCoordsY;
-// 	        var marginL=$imagePan_panning.css("marginLeft").replace("px", "");
-// 	        var marginT=$imagePan_panning.css("marginTop").replace("px", "");
-// 	        var animSpeed=500; //ease amount
-// 	        var easeType="easeOutCirc";
-// 	        if(mouseCoordsX>destX || mouseCoordsY>destY){
-// 	            $imagePan_container.stop().animate({left: -thePosA-marginL, top: -thePosC-marginT}, animSpeed,easeType); //with easing
-// 	        } else if(mouseCoordsX<destX || mouseCoordsY<destY){
-// ］	            $imagePan_container.stop().animate({left: thePosB-marginL, top: thePosD-marginT}, animSpeed,easeType); //with easing
-// 	        } else {
-// 	            $imagePan_container.stop();
-// 	        }
-// 	    }
-	 
-// 	    $imagePan_panning.css("margin-left",($imagePan.width()-$imagePan_panning.width())/2).css("margin-top",($imagePan.height()-$imagePan_panning.height())/2);
-	 
-// 	    $imagePan.bind("mousemove", function(event){
-// 	        MouseMove(event);
-// 	    });
-	// });
-
-	// $(window).resize(function() {
-	//     $imagePan.unbind("mousemove");
-	//     $imagePan_container.css("top",0).css("left",0);
-	//     $(window).load();
-	// });
-
-
-    // Place your code here.
-
+	});
   }
 };
 
