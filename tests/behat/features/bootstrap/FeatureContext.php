3efe5219172dbc1c261a8b6b2c3dab6bb3889fe5
Leonid Makarov <leonid.makarov@blinkreaction.com>
2015-04-07 13:28:32 -0400

    More behat tests; beat.yml now excluded from the repo; common settings in beta.commmon.yml

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -10,6 +10,8 @@ use Behat\Gherkin\Node\TableNode;
  */
 class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
+  protected $screenshot_dir = '/tmp';
+
   /**
    * Initializes context.
    *
@@ -17,7 +19,27 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    * You can also pass arbitrary arguments to the
    * context constructor through behat.yml.
    */
-  public function __construct() {
+  public function __construct($parameters) {
+    $this->parameters = $parameters;
+    if (isset($parameters['screenshot_dir'])) {
+      $this->screenshot_dir = $parameters['screenshot_dir'];
+    }
+  }
+
+  /**
+   * Take screenshot when step fails. Works only with Selenium2Driver.
+   * Screenshot is saved at [Date]/[Feature]/[Scenario]/[Step].jpg
+   *  @AfterStep
+   */
+  public function after(Behat\Behat\Hook\Scope\AfterStepScope $scope) {
+    if ($scope->getTestResult()->getResultCode() === 99) {
+      $driver = $this->getSession()->getDriver();
+      if ($driver instanceof Behat\Mink\Driver\Selenium2Driver) {
+        $fileName = date('d-m-y') . '-' . uniqid() . '.png';
+        $this->saveScreenshot($fileName, $this->screenshot_dir);
+        print 'Screenshot at: '.$this->screenshot_dir.'/' . $fileName;
+      }
+    }
   }
 
 }
