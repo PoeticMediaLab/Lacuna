e144bee1deaeed265b06f65001037299a331585b
Tim Loudon <tim@loudonco.com>
2015-05-11 09:32:18 -0700

    copyright updates
    create access permissions and a token system to limit course addition only to authorized users w/ the authcode
    simple ajax taxo creation
    stub for material addition

--- sites/all/modules/custom/course/course.js
+++ sites/all/modules/custom/course/course.js
@@ -1,7 +1,25 @@
 (function ($, Drupal, window, document, undefined) {
   Drupal.behaviors.create_course = {
     attach: function(context, settings) {
+      // createcourse-2
       $('#edit-field-registered-students-und').click(function() { $(".form-field-name-field-unregistered-students").toggle();});
+
+      // createcourse-3
+      terms = [];
+      $(".term").each(function() {
+        terms.push($(this).text());
+      });
+      $('#ajax-units').click(function() {
+        if (new_term = $('#edit-term').val()) {
+          $.get("/ajax/add-unit-term/" + new_term, function() {});
+          if (terms.indexOf(new_term) == -1) {
+            $(".terms").append("<div class='term'>" + new_term + "</div>");
+            terms.push(new_term);
+          }
+          $('#edit-term').val('');
+        }
+        return false; // dont submit the form
+      });
     }
   };
 })(jQuery, Drupal, this, this.document);
