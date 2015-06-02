e4361cabc6a85bca52f2a7464b31cb3710345d0f
Mike Widner <mikewidner@gmail.com>
2015-06-02 23:35:40 +0000

    Starting on javascript

---
+++ sites/all/modules/custom/page_turner/js/page_turner.js
@@ -0,0 +1,10 @@
+/**
+ * Page Turner hides/shows parts of a node
+ * based on settings about character length
+ * Here's where all the display work is done
+ *
+ **/
+
+if (Drupal.settings.page_turner_active) {
+  console.log("I loaded!");
+}
