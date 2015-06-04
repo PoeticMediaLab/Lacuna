c45ba5fcbeaba9f0598d657ef14ddfc9eebee3b1
Mike Widner <mikewidner@gmail.com>
2015-06-04 01:18:44 +0000

    Drupal.settings refactor

--- sites/all/modules/custom/page_turner/js/page_turner.js
+++ sites/all/modules/custom/page_turner/js/page_turner.js
@@ -8,52 +8,19 @@
  "use strict";
 
 (function($) {
-
   Drupal.behaviors.page_turner = {
     attach: function (context, settings) {
 
       var pages = Array();  // Used by multiple functions
       var cur_page = 1;     // current page
 
-      function split_breaks(content) {
-        // Split into pages based on the breaks in content
-      }
-
       function chunk_pages(content) {
         // Divide total content length by page length
         // And look for page break elements
         // Return array of page chunks
-        // var breaks = Array();
-        // var elements = settings.page_turner_breaks.split(',');
-        // var i;
-        // for (i = 0; i < elements.length; i++) {
-        //   breaks.push(content.getElementsByTagName(elements[i]));
-        // }
-        // // Logic:
-        // // Pages are all content from break (inclusive) to either
-        // // next break OR pagelength # characters
-        // elements = Array(); // done with that variable; re-use name
-        // for (i = 0; i < breaks.length; i++) {
-        //   // convert from array of HTMLCollections to array of objects
-        //   elements = elements.concat([].slice.call(breaks[i]));
-        // }
-        // for (i = 0; i < elements.length; i++) {
-        //   var chunks = $(elements[i]).nextUntil(elements[i + 1]);
-        //   var j;
-        //   var t_len = 0;
-        //   var html = Array(elements[i]);
-
-        //   // Join all HTML elements for this chunk
-        //   html = html.concat([].slice.call(chunks));
-
-        //   /*
-        //     Divide chunk into pages, if needed
-        //     Count text length against character limit
-        //   */
         var page = Array(); // holds all elements in a page
-        var t_len = 0;
-
-        var breaks = settings.page_turner_breaks.toLowerCase().split(',');
+        var t_len = 0;  // text length
+        var breaks = settings.page_turner.breaks.toLowerCase().split(',');
 
         // Get to the real content
         while (content.childElementCount == 1) {
@@ -65,7 +32,7 @@
         // and that adding it wouldn't go over the text limit
         var l = content.length;
         for (i = 0; i < l; i++) {
-          var is_break = (breaks.indexOf(content[i].tagName.toLowerCase()) != -1 );
+          var is_break = (breaks.indexOf(content[i].tagName.toLowerCase()) != -1);
           var new_len = t_len + content[i].textContent.length;
 
           if (is_break) {
@@ -79,7 +46,7 @@
               t_len = 0;
               page = Array(content[i]); // Include in the new page
             }
-          } else if (new_len <= settings.page_turner_pagelength) {
+          } else if (new_len <= settings.page_turner.page_length) {
             t_len = new_len;
             page.push(content[i]);
           } else if (page.length > 0) {
@@ -96,7 +63,6 @@
 
       // Class set in page_turner.css
       function hide_page(page_num) {
-        console.log(pages, page_num);
         $(pages[page_num]).addClass('page-turner-hidden');
       }
 
@@ -130,11 +96,11 @@
         // Add the pagers and listeners to both sides of page
       }
 
-      if (settings.page_turner_active) {
+      if (settings.page_turner.active) {
         // We assume here the body text is always the first field
         var content = context.getElementsByTagName('article')[0].getElementsByClassName('field')[0];
         var page_length = content.innerHTML.length;
-        if (page_length >= settings.page_turner_minlength) {
+        if (page_length >= settings.page_turner.min_length) {
           add_pagers(content);
           var pages = chunk_pages(content);
           cur_page = get_current_page();
@@ -147,6 +113,5 @@
         }
       }
     }
-  };
-
+  }; // END: Drupal.behaviors.page_turner
 })(jQuery);
