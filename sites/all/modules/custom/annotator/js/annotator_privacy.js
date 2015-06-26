5f0ca73b0b607f680816c20c24fee8d527ac9ba9
Tim Loudon <tim@loudonco.com>
2015-06-26 13:55:21 -0700

    tmp push

--- sites/all/modules/custom/annotator/js/annotator_privacy.js
+++ sites/all/modules/custom/annotator/js/annotator_privacy.js
@@ -4,16 +4,19 @@
       Drupal.Annotator.annotator('addPlugin', 'Privacy');
       if ($.isFunction($(document).on)) { // workaround for /admin using jquery 1.5.1 (otherwise, f() does not exist error)
         // need to use .on for future DOM elements, 1.5.1 has .delegate
-        $(document).on( "change", "form.annotator-widget .privacy.types input", function(e) {
-          if ("Private" == $(this).attr("id") && $(this).is(":checked")) {
-            $(".privacy.types #Instructor, .privacy.types #Co-Learners").prop('checked', false);
-            $(".annotator-widget .privacy.groups").hide(500);
-          }
+        $(document).on( "click", "form.annotator-widget .privacy.types .privacy-type", function(e) {
+          $(this).toggleClass("checked");
           if ("Co-Learners" == $(this).attr("id")) {
             $(".annotator-widget .privacy.groups").toggle(500);
           }
-          if (("Instructor" == $(this).attr("id") || "Co-Learners" == $(this).attr("id")) && $(this).is(":checked")) {
-            $(".privacy.types #Private").prop('checked', false);
+          if ($(this).hasClass("checked")) {
+            if ("Private" == $(this).attr("id")) {
+              $(".privacy.types #Instructor, .privacy.types #Co-Learners").removeClass('checked');
+              $(".annotator-widget .privacy.groups").hide(500);
+            }
+            if ("Instructor" == $(this).attr("id") || "Co-Learners" == $(this).attr("id")) {
+              $(".privacy.types #Private").removeClass('checked');
+            }
           }
         });
       }
