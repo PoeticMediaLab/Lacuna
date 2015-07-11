5542cbb5c0bbaf1f56d76ba383d06540446d6002
vagrant <vagrant@precise32.(none)>
2015-07-11 01:03:30 +0000

    Added Sass stylesheet for mobile CSS edits; fixed the issue where materials links would only work after two taps.

--- sites/all/themes/lacuna_stories/js/script.js
+++ sites/all/themes/lacuna_stories/js/script.js
@@ -14,10 +14,13 @@
 
 
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
-Drupal.behaviors.my_custom_behavior = {
+Drupal.behaviors.mobileSupport = {
   attach: function(context, settings) {
 
-    // Place your code here.
+    $('.view-display-id-materials .field-content a').mouseover(function(event){
+        $(event.target).trigger('click');
+        console.log('Hovered!');
+    });
 
   }
 };
