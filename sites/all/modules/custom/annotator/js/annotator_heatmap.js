57f48e8b7784fe5153c13bbd573b41a545fdf0d1
Mike Widner <mikewidner@gmail.com>
2015-07-30 19:02:59 +0000

    Removed old heatmap files

--- sites/all/modules/custom/annotator/js/annotator_heatmap.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  Drupal.behaviors.annotatorHeatmap = {
-    attach: function (context, settings) {
-      Drupal.Annotator.annotator('addPlugin', 'Heatmap', Drupal.settings.annotator_heatmap);
-    }
-  };
-})(jQuery);
