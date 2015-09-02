a843667a691bd23396a0d81b93a9c719e3d7b447
Mike Widner <mikewidner@gmail.com>
2015-09-02 19:42:30 +0000

    Swapping in CKEditor Full to get text justification

--- sites/all/libraries/ckeditor/styles.js
+++ sites/all/libraries/ckeditor/styles.js
@@ -19,28 +19,29 @@ CKEDITOR.stylesSet.add( 'default', [
 	// These styles are already available in the "Format" combo ("format" plugin),
 	// so they are not needed here by default. You may enable them to avoid
 	// placing the "Format" combo in the toolbar, maintaining the same features.
+	/*
+	{ name: 'Paragraph',		element: 'p' },
+	{ name: 'Heading 1',		element: 'h1' },
+	{ name: 'Heading 2',		element: 'h2' },
+	{ name: 'Heading 3',		element: 'h3' },
+	{ name: 'Heading 4',		element: 'h4' },
+	{ name: 'Heading 5',		element: 'h5' },
+	{ name: 'Heading 6',		element: 'h6' },
+	{ name: 'Preformatted Text',element: 'pre' },
+	{ name: 'Address',			element: 'address' },
+	*/
 
-	{ name: 'Body Text',		element: 'p' },
-	//{ name: 'Heading 1',		element: 'h1' },
-	{ name: 'Chapter Title',		element: 'h2' },
-	//{ name: 'Heading 3',		element: 'h3' },
-	{ name: 'Section Title',		element: 'h4' },
-	//{ name: 'Heading 5',		element: 'h5' },
-	//{ name: 'Heading 6',		element: 'h6' },
-	//{ name: 'Preformatted Text',element: 'pre' },
-	//{ name: 'Address',			element: 'address' },
-
-	//{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
-	//{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
-	//{
-	//	name: 'Special Container',
-	//	element: 'div',
-	//	styles: {
-	//		padding: '5px 10px',
-	//		background: '#eee',
-	//		border: '1px solid #ccc'
-	//	}
-	//},
+	{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
+	{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
+	{
+		name: 'Special Container',
+		element: 'div',
+		styles: {
+			padding: '5px 10px',
+			background: '#eee',
+			border: '1px solid #ccc'
+		}
+	},
 
 	/* Inline Styles */
 
@@ -56,55 +57,55 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: 'Superscript',		element: 'sup' },
 	*/
 
-	//{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
-    //
-	//{ name: 'Big',				element: 'big' },
-	//{ name: 'Small',			element: 'small' },
-	//{ name: 'Typewriter',		element: 'tt' },
-    //
-	//{ name: 'Computer Code',	element: 'code' },
-	//{ name: 'Keyboard Phrase',	element: 'kbd' },
-	//{ name: 'Sample Text',		element: 'samp' },
-	//{ name: 'Variable',			element: 'var' },
-    //
-	//{ name: 'Deleted Text',		element: 'del' },
-	//{ name: 'Inserted Text',	element: 'ins' },
+	{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
+
+	{ name: 'Big',				element: 'big' },
+	{ name: 'Small',			element: 'small' },
+	{ name: 'Typewriter',		element: 'tt' },
+
+	{ name: 'Computer Code',	element: 'code' },
+	{ name: 'Keyboard Phrase',	element: 'kbd' },
+	{ name: 'Sample Text',		element: 'samp' },
+	{ name: 'Variable',			element: 'var' },
+
+	{ name: 'Deleted Text',		element: 'del' },
+	{ name: 'Inserted Text',	element: 'ins' },
 
 	{ name: 'Cited Work',		element: 'cite' },
 	{ name: 'Inline Quotation',	element: 'q' },
 
-	//{ name: 'Language: RTL',	element: 'span', attributes: { 'dir': 'rtl' } },
-	//{ name: 'Language: LTR',	element: 'span', attributes: { 'dir': 'ltr' } },
+	{ name: 'Language: RTL',	element: 'span', attributes: { 'dir': 'rtl' } },
+	{ name: 'Language: LTR',	element: 'span', attributes: { 'dir': 'ltr' } },
 
 	/* Object Styles */
-    //
-	//{
-	//	name: 'Styled image (left)',
-	//	element: 'img',
-	//	attributes: { 'class': 'left' }
-	//},
-    //
-	//{
-	//	name: 'Styled image (right)',
-	//	element: 'img',
-	//	attributes: { 'class': 'right' }
-	//},
-    //
-	//{
-	//	name: 'Compact table',
-	//	element: 'table',
-	//	attributes: {
-	//		cellpadding: '5',
-	//		cellspacing: '0',
-	//		border: '1',
-	//		bordercolor: '#ccc'
-	//	},
-	//	styles: {
-	//		'border-collapse': 'collapse'
-	//	}
-	//},
-    //
-	//{ name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
-	//{ name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } }
+
+	{
+		name: 'Styled image (left)',
+		element: 'img',
+		attributes: { 'class': 'left' }
+	},
+
+	{
+		name: 'Styled image (right)',
+		element: 'img',
+		attributes: { 'class': 'right' }
+	},
+
+	{
+		name: 'Compact table',
+		element: 'table',
+		attributes: {
+			cellpadding: '5',
+			cellspacing: '0',
+			border: '1',
+			bordercolor: '#ccc'
+		},
+		styles: {
+			'border-collapse': 'collapse'
+		}
+	},
+
+	{ name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
+	{ name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } }
 ] );
 
