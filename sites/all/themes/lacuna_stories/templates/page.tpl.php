745ffaf65b92813ccb9e723e32f7d24bbc5d1aec
Mike Widner <mikewidner@gmail.com>
2015-09-11 20:14:01 +0000

    Added course name to page title

--- sites/all/themes/lacuna_stories/templates/page.tpl.php
+++ sites/all/themes/lacuna_stories/templates/page.tpl.php
@@ -36,17 +36,24 @@
 
   </header>
 
-  <div id="course-name">
+  <div id="subtitle">
     <?php if ($site_name || $site_slogan): ?>
     <div class="header__name-and-slogan" id="name-and-slogan">
       <?php if ($site_name): ?>
         <h1 class="header__site-name" id="site-name">
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?>
+        <?php
+            if (!drupal_is_front_page() && module_exists('course')):
+              $gid = course_get_selected_course();
+              $course = node_load($gid);
+        ?>
+          <a href="<?php print drupal_get_path_alias('node/' . $course->nid); ?>" title="<?php print t('About this Course'); ?>"></span><span id="course-name"><?php print $course->title; ?>
+        <?php endif; ?>
+          </span></a>
         </h1>
       <?php endif; ?>
 
       <?php if ($site_slogan):
-        // TODO: Add title of currently selected course
         ?>
         <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
       <?php endif; ?>
