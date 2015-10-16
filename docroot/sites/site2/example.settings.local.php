46c6bee708a5819935f946051fdfaf0681828b96
Leonid Makarov <leonid.makarov@ffwagency.com>
2015-10-16 10:19:05 -0400

    DB connection update

--- docroot/sites/site2/example.settings.local.php
+++ docroot/sites/site2/example.settings.local.php
@@ -1,20 +1,12 @@
 <?php
 
 # Docker DB connection settings.
-$databases = array (
-  'default' =>
-    array (
-      'default' =>
-        array (
-          'database' => 'site2',
-          'username' => getenv('DB_1_ENV_MYSQL_USER'),
-          'password' => getenv('DB_1_ENV_MYSQL_PASSWORD'),
-          'host' => getenv('DB_1_PORT_3306_TCP_ADDR'),
-          'port' => '',
-          'driver' => 'mysql',
-          'prefix' => '',
-        ),
-    ),
+$databases['default']['default'] = array (
+  'database' => 'site2',
+  'username' => getenv('DB_1_ENV_MYSQL_USER'),
+  'password' => getenv('DB_1_ENV_MYSQL_PASSWORD'),
+  'host' => getenv('DB_1_PORT_3306_TCP_ADDR'),
+  'driver' => 'mysql',
 );
 
 # File system settings.
