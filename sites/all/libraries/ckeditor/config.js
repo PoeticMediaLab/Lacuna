ad98f240cfd7693d4e2c88e0123fd61512f152ca
Mike Widner <mikewidner@gmail.com>
2016-07-19 16:22:34 +0000

    Strip out empty paragraphs from pasted content

--- sites/all/libraries/ckeditor/config.js
+++ sites/all/libraries/ckeditor/config.js
@@ -7,4 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.fillEmptyBlocks = false;
+	config.enterMode = CKEDITOR.ENTER_BR;
+    config.ignoreEmptyParagraph = true;
 };
