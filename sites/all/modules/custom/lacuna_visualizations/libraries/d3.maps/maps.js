546ef0704bedcda065643aba19754597ae8e7f1e
ireneehsu <ihsu5595@stanford.edu>
2016-08-24 00:12:42 +0000

    version with nodes in the right colorsgit log! all debugging print_r on lacuna_visualization.module commented out , console.log on maps.js also commented out (so this is the clean version of the correctly-colored map)

--- sites/all/modules/custom/lacuna_visualizations/libraries/d3.maps/maps.js
+++ sites/all/modules/custom/lacuna_visualizations/libraries/d3.maps/maps.js
@@ -33,17 +33,7 @@
         color  = d3.scale.category20(),
 		drawDocumentCircle = settings.config.drawDocumentCircle;
 
-		console.log(settings);
-		console.log(links)
-		console.log(settings.nids);
-
 	var linkedNodesOnly = !drawDocumentCircle;
-	//console.log(links);
-	for(var i = 0; i < nodes.length; i++){
-		if (nodes.edges !== null) {
-			console.log(nodes.edges);
-		}
-	}
 
 
 		// TODO: so this probably should be an object or an
@@ -79,9 +69,8 @@
 						nodes[i].data.author = safeName;
 					}
 					if (nodes[i].data.author === safeName) {
-						console.log(nodes[i].data.document_abstract);
+
 						userNamesColors.push([safeName, color(colorsCounter % 20), nodes[i].data.u_id]);
-						console.log(colorsCounter + " " + colorsCounter%20 + " " + userNamesColors[userNamesColors.length - 1][0] + " " + userNamesColors[userNamesColors.length - 1][1] + " " + userNamesColors[userNamesColors.length - 1][2]);
 						// for debugging purposes.
 						colorsCounter++;
 						selectedUsers.push(nodes[i].data.u_id);
