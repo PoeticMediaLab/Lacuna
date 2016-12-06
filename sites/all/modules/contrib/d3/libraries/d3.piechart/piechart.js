3527ebea7e430343e742d7c1479baf0dfdb3b95d
mwidner <mikewidner@stanford.edu>
2016-12-06 22:38:53 +0000

    Fixed lacuna_visualizations_theme() to not overwrite variables and upgraded to latest -dev version of d3

--- sites/all/modules/contrib/d3/libraries/d3.piechart/piechart.js
+++ sites/all/modules/contrib/d3/libraries/d3.piechart/piechart.js
@@ -14,7 +14,6 @@
 (function($) {
 
   Drupal.d3.piechart = function (select, settings) {
-
     var wedges = settings.rows,
       // Each wedge has a label and a value
       key = wedges.map(function(d) { return String(d[0]); }),
