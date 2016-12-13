86e2d579da6a7d1f4a4913843b7920b3b37e188e
Leonid Makarov <leonid.makarov@ffwagency.com>
2016-12-13 00:35:35 -0800

    Avoid setting reverse proxy settings for php-cli

--- docroot/sites/default/example.settings.local.php
+++ docroot/sites/default/example.settings.local.php
@@ -15,15 +15,17 @@ $conf['file_temporary_path'] = '/tmp';
 $conf['file_chmod_directory'] = 0777;
 $conf['file_chmod_file'] = 0666;
 
-# Reverse proxy configuration (Drude's vhost-proxy)
-$conf['reverse_proxy'] = TRUE;
-$conf['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
-// HTTPS behind reverse-proxy
-if (
-  isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' &&
-  !empty($conf['reverse_proxy']) && in_array($_SERVER['REMOTE_ADDR'], $conf['reverse_proxy_addresses'])
-) {
-  $_SERVER['HTTPS'] = 'on';
-  // This is hardcoded because there is no header specifying the original port.
-  $_SERVER['SERVER_PORT'] = 443;
+# Reverse proxy configuration (Docksal's vhost-proxy)
+if (!drupal_is_cli()) {
+    $conf['reverse_proxy'] = TRUE;
+    $conf['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
+    // HTTPS behind reverse-proxy
+    if (
+        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' &&
+        !empty($conf['reverse_proxy']) && in_array($_SERVER['REMOTE_ADDR'], $conf['reverse_proxy_addresses'])
+    ) {
+        $_SERVER['HTTPS'] = 'on';
+        // This is hardcoded because there is no header specifying the original port.
+        $_SERVER['SERVER_PORT'] = 443;
+    }
 }
