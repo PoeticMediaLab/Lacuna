9b928e84557226f91ea023e158c8904d68a8e7f6
Leonid Makarov <leonid.makarov@ffwagency.com>
2017-07-21 15:14:13 -0700

    Use utf8mb4 encoding in MySQL by default
    Also update Drupal settings files from the latest release

--- docroot/sites/default/example.settings.local.php
+++ docroot/sites/default/example.settings.local.php
@@ -2,11 +2,13 @@
 
 # Docksal DB connection settings.
 $databases['default']['default'] = array (
+  'driver' => 'mysql',
   'database' => 'default',
   'username' => 'user',
   'password' => 'user',
   'host' => 'db',
-  'driver' => 'mysql',
+  'charset' => 'utf8mb4',
+  'collation' => 'utf8mb4_general_ci'
 );
 
 # File system settings.
@@ -17,15 +19,15 @@ $conf['file_chmod_file'] = 0666;
 
 # Reverse proxy configuration (Docksal vhost-proxy)
 if (!drupal_is_cli()) {
-    $conf['reverse_proxy'] = TRUE;
-    $conf['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
-    // HTTPS behind reverse-proxy
-    if (
-        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' &&
-        !empty($conf['reverse_proxy']) && in_array($_SERVER['REMOTE_ADDR'], $conf['reverse_proxy_addresses'])
-    ) {
-        $_SERVER['HTTPS'] = 'on';
-        // This is hardcoded because there is no header specifying the original port.
-        $_SERVER['SERVER_PORT'] = 443;
-    }
+  $conf['reverse_proxy'] = TRUE;
+  $conf['reverse_proxy_addresses'] = array($_SERVER['REMOTE_ADDR']);
+  // HTTPS behind reverse-proxy
+  if (
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' &&
+    !empty($conf['reverse_proxy']) && in_array($_SERVER['REMOTE_ADDR'], $conf['reverse_proxy_addresses'])
+  ) {
+    $_SERVER['HTTPS'] = 'on';
+    // This is hardcoded because there is no header specifying the original port.
+    $_SERVER['SERVER_PORT'] = 443;
+  }
 }
