ef04bdbbefff9234ff0bd7497d5d51d68e9b88cd
Cody M Leff <codymleff@gmail.com>
2017-10-27 18:18:48 -0700

    Updated how JS is injected for PDF viewer and set viewer to load the correct PDF on opening.

--- sites/all/modules/custom/lacuna_pdf_document/lacuna_pdf_document.js
+++ sites/all/modules/custom/lacuna_pdf_document/lacuna_pdf_document.js
@@ -1,10 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*
+*   This script loads a PDF into the pdf.js demo viewer iframe and
+*   provides an interface through which the PDF Annotator plugin
+*   can access the document content to enable PDF annotation.
+*/
 
-    var viewer = document.querySelector('iframe.pdf')
-    viewer.addEventListener('load', function() {
+(function() {
 
-        var pdfViewerApplication = viewer.contentWindow.PDFViewerApplication
 
+    /*
+    *   References to the PDFJS library and the PDFViewerApplication
+    *   inside the iframe.
+    */
+
+    var PDFJS, PDFViewerApplication
+
+
+    /*
+    *   Once the page and PDF-rendering iframe have loaded, gathers
+    *   references to the PDFJS library and PDFViewerApplication and
+    *   loads the content PDF.
+    */
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        var viewer = document.querySelector('iframe.pdf')
+        var viewer_src_url = Drupal.settings.lacuna_pdf_document.viewer_src_url
+        var pdf_url = Drupal.settings.lacuna_pdf_document.pdf_url
+        viewer.src = viewer_src_url + '?file='/* + encodeURIComponent(pdf_url)*/
+        viewer.addEventListener('load', function() {
+
+            PDFJS = viewer.contentWindow.PDFJS
+            PDFViewerApplication = viewer.contentWindow.PDFViewerApplication
+            PDFViewerApplication.open(pdf_url)
+            
+        })
+        
     })
-    
-})
+
+})()
